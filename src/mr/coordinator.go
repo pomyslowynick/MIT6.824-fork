ee61e2e6da7841d10739763637c556edf8f2e22d
pomyslowynick <dark0mlet@gmail.com>
2024-04-07 20:26:07 +0100

    Add worker read and split file code

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -1,33 +1,39 @@
 package mr
 
-import "log"
-import "net"
-import "os"
-import "net/rpc"
-import "net/http"
-
+import (
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
+	"os"
+)
 
 type Coordinator struct {
 	// Your definitions here.
-
+	FileList    []string
+	UnusedFiles []string
+	UsedFiles   []string
 }
 
 // Your code here -- RPC handlers for the worker to call.
+func (c *Coordinator) RequestTask(args *TaskRequest, reply *TaskReply) error {
+	reply.Filename = c.UnusedFiles[0]
+	c.UnusedFiles = c.UnusedFiles[1:]
+	c.UsedFiles = append(c.UsedFiles, c.UnusedFiles[0])
+	return nil
 
-//
+}
+
 // an example RPC handler.
 //
 // the RPC argument and reply types are defined in rpc.go.
-//
 func (c *Coordinator) Example(args *ExampleArgs, reply *ExampleReply) error {
 	reply.Y = args.X + 1
 	return nil
 }
 
-
-//
 // start a thread that listens for RPCs from worker.go
-//
 func (c *Coordinator) server() {
 	rpc.Register(c)
 	rpc.HandleHTTP()
@@ -41,29 +47,25 @@ func (c *Coordinator) server() {
 	go http.Serve(l, nil)
 }
 
-//
 // main/mrcoordinator.go calls Done() periodically to find out
 // if the entire job has finished.
-//
 func (c *Coordinator) Done() bool {
 	ret := false
 
 	// Your code here.
 
-
 	return ret
 }
 
-//
 // create a Coordinator.
 // main/mrcoordinator.go calls this function.
 // nReduce is the number of reduce tasks to use.
-//
 func MakeCoordinator(files []string, nReduce int) *Coordinator {
 	c := Coordinator{}
-
+	c.FileList = files
+	c.UnusedFiles = files
+	fmt.Println(files)
 	// Your code here.
-
 
 	c.server()
 	return &c
--- src/mr/worker.go
+++ src/mr/worker.go
@@ -1,33 +1,30 @@
 package mr
 
-import "fmt"
-import "log"
-import "net/rpc"
-import "hash/fnv"
+import (
+	"bufio"
+	"fmt"
+	"hash/fnv"
+	"log"
+	"net/rpc"
+	"os"
+	"strings"
+)
 
-
-//
 // Map functions return a slice of KeyValue.
-//
 type KeyValue struct {
 	Key   string
 	Value string
 }
 
-//
 // use ihash(key) % NReduce to choose the reduce
 // task number for each KeyValue emitted by Map.
-//
 func ihash(key string) int {
 	h := fnv.New32a()
 	h.Write([]byte(key))
 	return int(h.Sum32() & 0x7fffffff)
 }
 
-
-//
 // main/mrworker.go calls this function.
-//
 func Worker(mapf func(string, string) []KeyValue,
 	reducef func(string, []string) string) {
 
@@ -35,14 +32,41 @@ func Worker(mapf func(string, string) []KeyValue,
 
 	// uncomment to send the Example RPC to the coordinator.
 	// CallExample()
+	args := TaskRequest{}
+	reply := TaskReply{}
 
+	ok := call("Coordinator.RequestTask", &args, &reply)
+	if ok {
+		// reply.Y should be 100.
+		fmt.Printf("reply %v\n", reply.Filename)
+	} else {
+		fmt.Printf("call failed!\n")
+	}
+
+	openedFile, err := os.Open(reply.Filename)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	wordsMap := make(map[string]int)
+	scanner := bufio.NewScanner(openedFile)
+	for i := 0; i < 10 && scanner.Scan(); i++ {
+		splitLine := strings.Split(scanner.Text(), " ")
+		for _, word := range splitLine {
+			if len(word) == 0 {
+				break
+			}
+			wordsMap[word]++
+		}
+
+		fmt.Println(splitLine)
+		fmt.Println(wordsMap)
+	}
 }
 
-//
 // example function to show how to make an RPC call to the coordinator.
 //
 // the RPC argument and reply types are defined in rpc.go.
-//
 func CallExample() {
 
 	// declare an argument structure.
@@ -67,11 +91,9 @@ func CallExample() {
 	}
 }
 
-//
 // send an RPC request to the coordinator, wait for the response.
 // usually returns true.
 // returns false if something goes wrong.
-//
 func call(rpcname string, args interface{}, reply interface{}) bool {
 	// c, err := rpc.DialHTTP("tcp", "127.0.0.1"+":1234")
 	sockname := coordinatorSock()
